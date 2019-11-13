2598e090c27353c711af0fb86db87be80ee9ef2c
Shen Jiahuan <shenjiahuan@sjtu.edu.cn>
2019-11-13 14:26:56 +0800

    fix bugs in lab4

--- src/tiger/semant/semant.cc
+++ src/tiger/semant/semant.cc
@@ -68,7 +68,7 @@ TY::Ty *FieldVar::SemAnalyze(VEnvType venv, TEnvType tenv,
     list = list->tail;
   }
   errormsg.Error(this->pos, "field %s doesn't exist", this->sym->Name().c_str());
-  return TY::IntTy::Instance();
+  return TY::VoidTy::Instance();
 
 }
 
@@ -77,28 +77,27 @@ TY::Ty *SubscriptVar::SemAnalyze(VEnvType venv, TEnvType tenv,
   TY::Ty *ty = this->var->SemAnalyze(venv, tenv, labelcount)->ActualTy();
   if (ty->kind != TY::Ty::Kind::ARRAY) {
     errormsg.Error(this->pos, "array type required");
-    TY::VoidTy::Instance();
+    return TY::VoidTy::Instance();
   }
-  return ty;
+  return ((TY::ArrayTy *) ty)->ty->ActualTy();
 }
 
 TY::Ty *VarExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
   if (this->var->kind == Var::SIMPLE) {
     if (!venv->Look(((SimpleVar *) this->var)->sym)) {
       errormsg.Error(this->pos, "undefined variable %s", ((SimpleVar *) this->var)->sym->Name().c_str());
-      return TY::IntTy::Instance();
+      return TY::NilTy::Instance();
     }
-    return ((E::VarEntry *)venv->Look(((SimpleVar *) this->var)->sym))->ty;
+    return this->var->SemAnalyze(venv, tenv, labelcount);
   } else if (this->var->kind == Var::SUBSCRIPT) {
-    this->var->SemAnalyze(venv, tenv, labelcount);
+    return this->var->SemAnalyze(venv, tenv, labelcount);
   } else if (this->var->kind == Var::FIELD) {
-    this->var->SemAnalyze(venv, tenv, labelcount);
+    return this->var->SemAnalyze(venv, tenv, labelcount);
   }
-  return TY::VoidTy::Instance();
 }
 
 TY::Ty *NilExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
-  return TY::VoidTy::Instance();
+  return TY::NilTy::Instance();
 }
 
 TY::Ty *IntExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
@@ -126,41 +125,34 @@ TY::Ty *CallExp::SemAnalyze(VEnvType venv, TEnvType tenv,
   TY::Ty *result = ((E::FunEntry *)x)->result;
   ExpList *args;
   for (args = this->args; args && formals; args = args->tail, formals = formals->tail) {
-    TY::Ty *ty = args->head->SemAnalyze(venv, tenv, labelcount);
-    if (formals->head != ty) {
+    TY::Ty *ty = args->head->SemAnalyze(venv, tenv, labelcount)->ActualTy();
+    if (formals->head != ty && ty->kind != TY::Ty::Kind::NIL) {
       errormsg.Error(this->pos, "para type mismatch");
     }
   }
   if (args) {
     errormsg.Error(this->pos, "too many params in function %s", this->func->Name().c_str());
   }
-  if (formals) {
-    errormsg.Error(this->pos, "xxx");
-  }
-  if (result->kind == TY::Ty::Kind::INT) {
-    return TY::IntTy::Instance();
-  } else {
-    return TY::VoidTy::Instance();
-  }
+  return result;
 
 }
 
 TY::Ty *OpExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
-  TY::Ty *left = this->left->SemAnalyze(venv, tenv, labelcount);
-  TY::Ty *right = this->right->SemAnalyze(venv, tenv, labelcount);
+  TY::Ty *left = this->left->SemAnalyze(venv, tenv, labelcount)->ActualTy();
+  TY::Ty *right = this->right->SemAnalyze(venv, tenv, labelcount)->ActualTy();
 
   if (this->oper == PLUS_OP || this->oper == MINUS_OP || this->oper == TIMES_OP || this->oper == DIVIDE_OP) {
-    if (left->kind != TY::Ty::Kind::INT) {
+    if (left->kind != TY::Ty::Kind::INT && left->kind != TY::Ty::Kind::NIL) {
       errormsg.Error(this->left->pos, "integer required");
       return TY::IntTy::Instance();
     }
 
-    if (right->kind != TY::Ty::Kind::INT) {
+    if (right->kind != TY::Ty::Kind::INT && right->kind != TY::Ty::Kind::NIL) {
       errormsg.Error(this->right->pos, "integer required");
       return TY::IntTy::Instance();
     }
   } else {
-    if (!left->IsSameType(right)) {
+    if (!left->IsSameType(right) && left->kind != TY::Ty::Kind::VOID && right->kind != TY::Ty::Kind::VOID) {
       errormsg.Error(this->pos, "same type required");
     }
   }
@@ -190,9 +182,9 @@ TY::Ty *SeqExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
 
 TY::Ty *AssignExp::SemAnalyze(VEnvType venv, TEnvType tenv,
                               int labelcount) const {
-  TY::Ty *var = this->var->SemAnalyze(venv, tenv, labelcount);
-  TY::Ty *exp = this->exp->SemAnalyze(venv, tenv, labelcount);
-  if (var->kind != exp->kind) {
+  TY::Ty *var = this->var->SemAnalyze(venv, tenv, labelcount)->ActualTy();
+  TY::Ty *exp = this->exp->SemAnalyze(venv, tenv, labelcount)->ActualTy();
+  if (var->kind != exp->kind && var->kind != TY::Ty::Kind::VOID && exp->kind != TY::Ty::Kind::VOID) {
     errormsg.Error(this->pos, "unmatched assign exp");
     return TY::VoidTy::Instance();
   }
@@ -203,25 +195,25 @@ TY::Ty *AssignExp::SemAnalyze(VEnvType venv, TEnvType tenv,
       errormsg.Error(this->pos, "loop variable can't be assigned");
     }
   }
-  return var;
+  return TY::VoidTy::Instance();
 }
 
 TY::Ty *IfExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
-  TY::Ty *test = this->test->SemAnalyze(venv, tenv, labelcount);
-  TY::Ty *then = this->then->SemAnalyze(venv, tenv, labelcount);
+  TY::Ty *test = this->test->SemAnalyze(venv, tenv, labelcount)->ActualTy();
+  TY::Ty *then = this->then->SemAnalyze(venv, tenv, labelcount)->ActualTy();
   TY::Ty *elsee = nullptr;
   if (this->elsee) {
-    elsee = this->elsee->SemAnalyze(venv, tenv, labelcount);
+    elsee = this->elsee->SemAnalyze(venv, tenv, labelcount)->ActualTy();
   }
 
   if (!elsee && then->kind != TY::Ty::Kind::VOID) {
     errormsg.Error(this->pos, "if-then exp's body must produce no value");
   }
 
-  if (elsee && then->kind != elsee->kind) {
+  if (elsee && then->kind != elsee->kind && then->kind != TY::Ty::Kind::NIL && elsee->kind != TY::Ty::Kind::NIL) {
     errormsg.Error(this->pos, "then exp and else exp type mismatch");
   }
-  return TY::IntTy::Instance();
+  return then;
 }
 
 TY::Ty *WhileExp::SemAnalyze(VEnvType venv, TEnvType tenv,
@@ -230,7 +222,7 @@ TY::Ty *WhileExp::SemAnalyze(VEnvType venv, TEnvType tenv,
   if (body->kind != TY::Ty::Kind::VOID) {
     errormsg.Error(this->pos, "while body must produce no value");
   }
-  return TY::VoidTy::Instance();
+  return body;
 }
 
 TY::Ty *ForExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
@@ -263,7 +255,7 @@ TY::Ty *LetExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
     list = list->tail;
   }
   TY::Ty *body = this->body->SemAnalyze(venv, tenv, labelcount);
-  return TY::VoidTy::Instance();
+  return body;
 }
 
 TY::Ty *ArrayExp::SemAnalyze(VEnvType venv, TEnvType tenv,
@@ -272,7 +264,7 @@ TY::Ty *ArrayExp::SemAnalyze(VEnvType venv, TEnvType tenv,
 
   if (((TY::ArrayTy *)ty)->ty->ActualTy() != this->init->SemAnalyze(venv, tenv, labelcount)->ActualTy()) {
     errormsg.Error(this->pos, "type mismatch");
-    return TY::VoidTy::Instance();
+    return ty;
   }
   return tenv->Look(this->typ);
 }
@@ -286,16 +278,19 @@ void FunctionDec::SemAnalyze(VEnvType venv, TEnvType tenv,
                              int labelcount) const {
   FunDecList* list = this->functions;
   FunDec* head;
+  VEnvType cur_venv = E::BaseVEnv();
   while (list) {
     head = list->head;
-    if (venv->Look(head->name)) {
+    if (cur_venv->Look(head->name)) {
       errormsg.Error(this->pos, "two functions have the same name");
     }
     TY::TyList *formalTyList = make_formal_tylist(tenv, head->params);
     if (head->result) {
       TY::Ty* resultTy = tenv->Look(head->result);
+      cur_venv->Enter(head->name, new E::FunEntry(formalTyList, resultTy));
       venv->Enter(head->name, new E::FunEntry(formalTyList, resultTy));
     } else {
+      cur_venv->Enter(head->name, new E::FunEntry(formalTyList, TY::VoidTy::Instance()));
       venv->Enter(head->name, new E::FunEntry(formalTyList, TY::VoidTy::Instance()));
     }
     list = list->tail;
@@ -311,7 +306,7 @@ void FunctionDec::SemAnalyze(VEnvType venv, TEnvType tenv,
     }
     TY::Ty *ty = head->body->SemAnalyze(venv, tenv, labelcount);
     E::EnvEntry *entry = venv->Look(head->name);
-    if (entry->kind != E::EnvEntry::Kind::FUN || ty != ((E::FunEntry *) entry)->result) {
+    if (entry->kind != E::EnvEntry::Kind::FUN || ty != ((E::FunEntry *) entry)->result->ActualTy()) {
       errormsg.Error(this->pos, "procedure returns value");
     }
     venv->EndScope();
@@ -321,10 +316,12 @@ void FunctionDec::SemAnalyze(VEnvType venv, TEnvType tenv,
 
 void VarDec::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
   TY::Ty *init = this->init->SemAnalyze(venv, tenv, labelcount)->ActualTy();
-  if (!this->typ && init->kind == TY::Ty::Kind::VOID) {
+  if (!this->typ && init->kind == TY::Ty::Kind::NIL) {
     errormsg.Error(this->pos, "init should not be nil without type specified");
+  } else if (this->typ && init->kind == TY::Ty::Kind::VOID) {
+      init = tenv->Look(this->typ)->ActualTy();
   }
-  if (this->typ && tenv->Look(this->typ)->ActualTy() != init) {
+  if (this->typ && tenv->Look(this->typ)->ActualTy() != init && init->kind != TY::Ty::Kind::NIL) {
     errormsg.Error(this->pos, "type mismatch");
   }
   venv->Enter(this->var, new E::VarEntry(init));
@@ -333,12 +330,13 @@ void VarDec::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
 void TypeDec::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
   NameAndTyList* list = this->types;
   NameAndTy* head;
+  TEnvType cur_tenv = E::BaseTEnv();
   while (list) {
     head = list->head;
-    if (tenv->Look(head->name)) {
+    if (cur_tenv->Look(head->name)) {
       errormsg.Error(this->pos, "two types have the same name");
     }
-
+    cur_tenv->Enter(head->name, new TY::NameTy(head->name, nullptr));
     tenv->Enter(head->name, new TY::NameTy(head->name, nullptr));
     list = list->tail;
   }
